6012c810f6db6f700118ad304aa0bbc7d71e73a5
minhpro5276 <101793825+minhpro5276@users.noreply.github.com>
2022-05-30 08:16:23 +0700

    Add project files.

---
+++ NguyenBinhMinh_Bigschool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenBinhMinh_Bigschool.MvcApplication" Language="C#" %>
---
+++ NguyenBinhMinh_Bigschool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NguyenBinhMinh_Bigschool.Startup))]
+namespace NguyenBinhMinh_Bigschool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
